7e9f75a665724f7e7d04956b2f6f4ebc5339d052
Victor Rak <rakvium@gmail.com>
2014-02-26 15:39:26 +0200

    precompile font awesome for heroku

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ module Blog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += ["fontawesome-webfont.ttf",
+                         "fontawesome-webfont.eot",
+                         "fontawesome-webfont.svg",
+                         "fontawesome-webfont.woff"]
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
