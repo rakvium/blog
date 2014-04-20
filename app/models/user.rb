d84f8a5113b7b289dcda605ac0b6e818b4a8b04f
rakvium <rakvium@gmail.com>
2014-04-20 13:48:49 +0300

    add uniquness validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-  
+  validates :email, :uniqueness => true
 
   def role?(role_name)
     self.roles.last == Role.where(:name=>role_name).last 
