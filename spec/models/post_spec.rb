feae163721438e4cb3789dedb57636c8dba6fc0b
Victor Rak <rakvium@gmail.com>
2017-02-01 11:29:27 +0200

    Add specs for Post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'relations' do
+    it { should belong_to(:user) }
+    it { should have_many(:comments).dependent(:destroy) }
+  end
+
+  describe 'vaidations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_least(5).is_at_most(255) }
+  end
+end
