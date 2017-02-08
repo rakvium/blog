fa11866ef2a12736344f5f39f8f5ae17ae8f9fa5
Victor Rak <rakvium@gmail.com>
2017-02-08 16:21:20 +0200

    Add UsersRole fabric

---
+++ spec/factories/users_roles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :users_role do
+    user
+    role { Role.find_or_create_by(name: :user) }
+  end
+end
--- spec/models/factories_spec.rb
+++ spec/models/factories_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Factories' do
   FactoryGirl.factories.map(&:name).each do |factory_name|
-    describe "The #{factory_name.capitalize} factory" do
+    describe "The #{factory_name} factory" do
       it 'is valid' do
         FactoryGirl.build(factory_name).should be_valid
       end
