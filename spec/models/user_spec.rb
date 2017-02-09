498466ffdf2c3833a97b08c7d97cc3c42971faac
Victor Rak <rakvium@gmail.com>
2017-02-09 12:31:43 +0200

    Add specs for User#role? method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
+
+  describe '#role?' do
+    let!(:user_role) { FactoryGirl.create(:role, name: :user) }
+    let!(:admin_role) { FactoryGirl.create(:role, name: :admin) }
+    let(:user) { FactoryGirl.create(:user) }
+    context 'for equal role' do
+      subject { user.role?(:user) }
+      it { should be_true }
+    end
+
+    context 'for non-equal role' do
+      subject { user.role?(:admin) }
+      it { should be_false }
+    end
+  end
 end
