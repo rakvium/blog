496e6cfb08fff6a4d13be5661de5069f06125c20
Victor Rak <rakvium@gmail.com>
2017-10-17 20:32:21 +0300

    Add full name and datetime format helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,10 @@
 # helpers for PostsController
 module PostsHelper
+  def full_name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  def my_date_format(date)
+    date.strftime('%d %B %Y at %H:%M')
+  end
 end
