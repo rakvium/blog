90df799ab4da8a4b57a28c3862a003f61e9ee81e
Viktor Rak <v.rak@sloboda-studio.com>
2018-10-08 12:46:26 +0300

    Set up SendGrid as SMTP service for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,15 @@ Blog::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'hello-blog.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
