ce4a9a5fa2881a71c1b03aa4802df05805bd1c2f
Sudakov Eugene <sudakoveugene@gmail.com>
2018-10-09 00:47:49 +0300

    Simplify spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
-# Require simplecov to check tests coverage
 if ENV['CI'] == 'true'
   require 'simplecov'
   SimpleCov.start
 end
 
-# Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'rspec/rails'
@@ -31,19 +28,11 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-  config.infer_spec_type_from_file_location!
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
