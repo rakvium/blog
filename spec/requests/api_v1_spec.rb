3cc1133c2573744d3300d1d533ce71be2035456e
Sudakov Eugene <sudakoveugene@gmail.com>
2018-10-10 00:53:03 +0300

    Fix request posts specs

--- spec/requests/api_v1_spec.rb
+++ spec/requests/api_v1_spec.rb
@@ -2,20 +2,22 @@
 
 require 'spec_helper'
 
-RSpec.describe API, type: :request do
+describe API, type: :request do
   describe 'GET /api/v1/posts' do
-    context 'no posts created' do
+    context 'when no posts created' do
       it 'returns an empty array' do
         get '/api/v1/posts'
-        response.body.should == MultiJson.encode([])
+        expect(JSON.parse(response.body)['posts']).to eq []
       end
     end
-    context 'post created' do
-      let!(:post) { create(:post) }
 
-      it 'returns array with posts' do
+    context 'when post created' do
+      let!(:post) { create(:post) }
+      let(:represented_post) { JSON.parse(API::V1::Entities::Posts.represent(post).to_json) }
+
+      it 'returns array with created post' do
         get '/api/v1/posts'
-        response.body.should eq MultiJson.encode([post])
+        expect(JSON.parse(response.body)['posts']).to include(represented_post)
       end
     end
   end
