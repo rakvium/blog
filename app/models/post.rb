cd0391d603b83b62371344faff5561315f16eb5b
Sudakov Eugene <sudakoveugene@gmail.com>
2018-10-10 13:19:44 +0300

    Add todo for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  # TODO: Check deeply these relations and, probably, refactor posts at all
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
