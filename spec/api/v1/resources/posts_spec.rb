e821a1229a7f5049aba4d2e8714b3b306311ae5d
Viktor Rak <v.rak@sloboda-studio.com>
2018-11-29 20:01:57 +0200

    Add specs for API posts endpoint

---
+++ spec/api/v1/resources/posts_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe API::V1::Resources::Posts do
+  include Rack::Test::Methods
+
+  def app
+    API::V1::Resources::Posts
+  end
+
+  describe 'GET /api/v1/posts' do
+    let(:expected_response) { { posts: Post.all }.to_json }
+
+    before { get '/api/v1/posts' }
+
+    it 'responds with 200' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'returns all the posts' do
+      expect(last_response.body).to eq(expected_response)
+    end
+  end
+end
