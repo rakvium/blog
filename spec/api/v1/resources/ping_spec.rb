841d6b91a940e8e024042024e14f027086918308
Viktor Rak <v.rak@sloboda-studio.com>
2018-11-29 20:01:22 +0200

    Add specs for API ping

---
+++ spec/api/v1/resources/ping_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe API::V1::Resources::Ping do
+  include Rack::Test::Methods
+
+  def app
+    API::V1::Resources::Ping
+  end
+
+  describe 'GET /ping' do
+    # TODO: check if response should actually be like this
+    let(:expected_response) { { result: 'pong' }.to_s }
+
+    before { get '/ping' }
+
+    it 'responds with 200' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'returns a pong' do
+      expect(last_response.body).to eq(expected_response)
+    end
+  end
+end
