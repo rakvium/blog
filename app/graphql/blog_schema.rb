d759644c9eebfa8874111c0badb4b0039eae690a
Eugene Sudakov <sudakoveugene@gmail.com>
2020-10-30 15:15:03 +0300

    Add login and sign out graphql endpoints for users

--- app/graphql/blog_schema.rb
+++ app/graphql/blog_schema.rb
@@ -7,6 +7,10 @@ class BlogSchema < GraphQL::Schema
   use GraphQL::Pagination::Connections
   use GraphQL::Execution::Errors
 
+  rescue_from(ActiveRecord::RecordNotSaved) do |err|
+    raise GraphQL::ExecutionError, err.message
+  end
+
   def self.unauthorized_object(_error)
     raise GraphQL::ExecutionError, I18n.t('authentication.failure.unauthenticated')
   end
---
+++ app/graphql/mutations/users/login.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Users
+    class Login < Mutations::BaseMutation
+      description 'Login for users'
+
+      argument :email, String, required: true
+      argument :password, String, required: true
+
+      field :user, Types::UserType, null: false
+      field :token, String, null: false
+
+      def resolve(args = {})
+        user = User.find_for_authentication(email: args[:email])
+
+        if user.present?
+          if user.valid_password?(args[:password])
+            {
+              user: user,
+              token: TokenManager::TokenCreator.call(user, context[:request])
+            }
+          else
+            GraphQL::ExecutionError.new(I18n.t('authentication.failure.invalid'))
+          end
+        else
+          GraphQL::ExecutionError.new(I18n.t('authentication.failure.not_found_in_database'))
+        end
+      end
+
+      def self.authorized?(_object, context)
+        return true if context[:current_user].blank?
+
+        raise GraphQL::ExecutionError, I18n.t('authentication.failure.already_signed_in')
+      end
+    end
+  end
+end
