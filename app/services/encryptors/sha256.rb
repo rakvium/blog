ab378a2ea47ff7bd99b00bea518d1a869977ceee
Viktor Rak <rakvium@gmail.com>
2021-06-13 20:43:38 +0300

    Set up default secret encryptor key salt
    for development and test environments

--- app/services/encryptors/sha256.rb
+++ app/services/encryptors/sha256.rb
@@ -2,7 +2,7 @@
 
 module Encryptors
   class Sha256
-    def self.call(value, salt = ENV['SECRET_ENCRYPTOR_KEY_SALT'])
+    def self.call(value, salt = ENV.fetch('SECRET_ENCRYPTOR_KEY_SALT'))
       OpenSSL::HMAC.hexdigest('SHA256', salt, value)
     end
   end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Set up a default secret enrcyptor key salt for development and test environments
+ENV['SECRET_ENCRYPTOR_KEY_SALT'] ||= 'verysecretsalt' if Rails.env.development? || Rails.env.test?
+# Check if secret encryptor key salt is set to avoid server errors in runtime
+ENV.fetch('SECRET_ENCRYPTOR_KEY_SALT')
+
 # Initialize the Rails application.
 Rails.application.initialize!
