0f79e819db6e9699dc9f4befdc2be30205ef4f37
Eugene Sudakov <sudakoveugene@gmail.com>
2021-10-31 15:36:52 +0200

    Synchronize posts' constraints with model validations

---
+++ db/migrate/20211031132101_change_posts_constraints.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class ChangePostsConstraints < ActiveRecord::Migration[6.0]
+  # rubocop:disable Metrics/MethodLength
+  def change
+    change_column_null(:posts, :title, false)
+    add_foreign_key :posts, :users, on_delete: :cascade
+
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL.squish
+          ALTER TABLE posts
+            ADD CONSTRAINT check_posts_title_length
+              CHECK (char_length(title) BETWEEN 5 AND 255);
+        SQL
+      end
+
+      dir.down do
+        execute <<-SQL.squish
+          ALTER TABLE posts
+            DROP CONSTRAINT check_posts_title_length;
+        SQL
+      end
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
+end
