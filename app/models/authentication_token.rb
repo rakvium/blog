3e214bdcc0dc77f6b03967d768879bdd8a01a895
Eugene Sudakov <sudakoveugene@gmail.com>
2021-10-31 15:08:23 +0200

    Synchronize authentication_tokens' constraints with model validations

--- app/models/authentication_token.rb
+++ app/models/authentication_token.rb
@@ -4,4 +4,5 @@ class AuthenticationToken < ApplicationRecord
   belongs_to :user
 
   validates :user_agent, :ip_address, :last_used_at, :value_digest, presence: true
+  validates :value_digest, uniqueness: true
 end
